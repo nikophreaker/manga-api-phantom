70d03a9f17f53342d62bc48fdedf0ff7df8c224b
nikophreaker <nikx449@gmail.com>
2020-12-19 23:44:03 +0700

    phantom setting log result/error

--- scrappers/phantom-script.js
+++ scrappers/phantom-script.js
@@ -3,7 +3,7 @@ var env = system.env;
 var page = require("webpage").create();
 
 page.settings.userAgent =
-  "Mozilla/5.0";
+  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36";
 
 // default viewport size is small, change it to 1366x768
 page.viewportSize = {
@@ -65,6 +65,7 @@ page.open(env.URL, function(status) {
 
         // exit and return HTML
         system.stdout.write(result);
+        console.log(result);
         });
         phantom.exit(0);
       } else {
@@ -76,6 +77,7 @@ page.open(env.URL, function(status) {
   } else {
     // if status is not 'success' exit with an error
     system.stderr.write(error);
+    console.log(error);
     phantom.exit(1);
   }
 });
