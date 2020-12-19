d95a2a6ff4630628fc0f9c5572a43dd4fcf7f32f
nikophreaker <nikx449@gmail.com>
2020-12-19 23:16:51 +0700

    user-agent changed

--- scrappers/phantom-script.js
+++ scrappers/phantom-script.js
@@ -3,7 +3,7 @@ var env = system.env;
 var page = require("webpage").create();
 
 page.settings.userAgent =
-  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36";
+  "Mozilla/5.0";
 
 // default viewport size is small, change it to 1366x768
 page.viewportSize = {
