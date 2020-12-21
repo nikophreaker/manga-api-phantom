42ac915a08d8c7c2f232daabb11e58c29fd8bdee
nikophreaker <nikx449@gmail.com>
2020-12-21 09:33:02 +0700

    using axios

--- routers/manga.js
+++ routers/manga.js
@@ -23,10 +23,11 @@ router.get("/manga/page/:pagenumber", async (req, res)=> {
   `https://bacakomik.co/daftar-manga/page/${pagenumber}/`;
   
   try {
-//     const response = await axios.get(url, 
-//       { headers: {
-//       'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36' }  });
-//     console.log(url);
+    const response = await axios.get(url, 
+      { headers: {
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36' }  });
+    console.log(url);
+    console.log(response);
     fetch(
         url,
         error => {
